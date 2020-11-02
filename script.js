3b187b62a7d308b365d3debdc741382a943eeb86
Yolanda <yolandaoktakhania@gmail.com>
2020-11-02 22:38:49 +0700

    preloader

---
+++ script.js
@@ -0,0 +1,52 @@
+var myVar;
+
+
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3500);
+  }
+
+function showPage(){
+    document.getElementById("preload").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+}
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml5 .line',
+    opacity: [0.5,1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700
+  }).add({
+    targets: '.ml5 .line',
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: (el, i) => (-1.6 + 1.5*2*i) + "em"
+  }).add({
+    targets: '.ml5 .hello',
+    opacity: [0,1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5 .letters-left',
+    opacity: [0,1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=300'
+  }).add({
+    targets: '.ml5 .letters-right',
+    opacity: [0,1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=600'
+  }).add({
+    targets: '.ml5',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
